2907b15857a9c767bb97a336c55dbc89e98fedd5
Carlos Jimenez <carlosclase37@gmail.com>
2023-03-01 17:52:56 +0100

    Demo recuperado

---
+++ src/com/iesam/openbank/presentation/Main.java
@@ -0,0 +1,13 @@
+package com.iesam.openbank.presentation;
+
+import com.iesam.openbank.domain.models.Cliente;
+
+public class Main {
+
+    public static void main(String[]Args){
+
+        Cliente cliente = new Cliente();
+        cliente.setNombre("pepe");
+    }
+
+}
